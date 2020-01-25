e027d7976105f5b49456d8f801842d339dfe908a
Roger Walker <roger.john.walker@gmail.com>
2020-01-25 14:14:14 -0500

    working environment... with a working example

--- getting-started/src/index.ts
+++ getting-started/src/index.ts
@@ -1,4 +1,9 @@
-import { Observable } from 'rxjs';
+import { Observable, fromEvent } from 'rxjs';
+import { map, distinctUntilChanged, debounceTime, mergeMap, switchMap } from 'rxjs/operators';
+import * as $ from 'jquery';
+
+const $search = $('#search');
+const $results = $('#results');
 
 let observable = Observable.create((observer: any) => {
     observer.next('Hello World!');
@@ -7,15 +12,44 @@ let observable = Observable.create((observer: any) => {
     observer.next('Bye');
 });
 
+const queries$ = fromEvent<KeyboardEvent>($search, 'keyup')
+    .pipe(
+        map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+        debounceTime(250),
+        distinctUntilChanged(),
+        switchMap(query => getItems(query)),
+    );
+
+
+queries$.subscribe((items : Array<string>)  => {
+    $results.empty();
+    $results.append(
+        items.map((item: string): JQuery<HTMLElement> => {
+            return $('<li />').text(item);
+        })
+    );
+});
+
+
 observable.subscribe(
-    (x:any) => logItem(x),
+    (x: any) => logItem(x),
     (error: any) => logItem('Error: ' + error),
     () => logItem('Completed')
 );
 
-function logItem(val:any){
+function logItem(val: any) {
     var node = document.createElement('li');
     var textnode = document.createTextNode(val);
     node.appendChild(textnode);
     document.getElementById('list').appendChild(node);
+}
+
+function getItems(search: string) {
+    console.log(`Querying ${search}`);
+
+    return new Promise((resolve, reject) => {
+        window.setTimeout(() => {
+            resolve([search, 'Item 2', `Another ${Math.random()}`]);
+        }, 500 + Math.random() * 5000);
+    });
 }
